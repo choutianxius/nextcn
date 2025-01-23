89c44f93d523853566ebad687c58b1a67db0f376
choutianxius <choutianxius@gmail.com>
2025-01-23 07:48:38 -0600

    Add shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import Counter from "@/components/counter";
+
 export default function Page() {
   return (
-    <div className="w-screen h-screen flex">
-      <p className="m-auto">Next.js + TailwindCSS</p>
+    <div className="w-screen h-screen flex flex-col justify-center items-center">
+      <p className="mb-2">Next.js + TailwindCSS</p>
+      <Counter />
     </div>
   );
 }
---
+++ components/counter.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+import { Button } from "@/components/ui/button";
+
+export default function Counter() {
+  const [count, setCount] = React.useState<number>(0);
+
+  return (
+    <Button variant={"outline"} onClick={() => setCount((x) => x + 1)}>
+      Clicked {count} times
+    </Button>
+  );
+}
