1fa41da15af5b7ad481f488a6b90276adf50d3ea
Tianxiu Zhou <choutianxius@gmail.com>
2025-02-21 17:06:54 -0600

    Use react compiler

--- components/counter.tsx
+++ components/counter.tsx
@@ -7,10 +7,7 @@ export default function Counter() {
   const [count, setCount] = React.useState<number>(0);
 
   return (
-    <Button
-      variant={"outline"}
-      onClick={() => setCount((x) => x + 1)}
-    >
+    <Button variant={"outline"} onClick={() => setCount((x) => x + 1)}>
       Clicked {count} times
     </Button>
   );
