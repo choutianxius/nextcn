aa7bed4dde4fa8a701b086e08c5a311abd57c28d
Tianxiu Zhou <choutianxius@gmail.com>
2025-03-21 15:17:10 -0500

    Bump

--- components/counter.tsx
+++ components/counter.tsx
@@ -7,7 +7,7 @@ export default function Counter() {
   const [count, setCount] = React.useState<number>(0);
 
   return (
-    <Button variant={"outline"} onClick={() => setCount((x) => x + 1)}>
+    <Button variant={"default"} onClick={() => setCount((x) => x + 1)}>
       Clicked {count} times
     </Button>
   );
